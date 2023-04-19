66a77f1531ba3404e8e19374d5612a73dcf58e76
carlospoma23 <carlospoma23@gmail.com>
2023-04-19 08:24:07 -0600

    finish week1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    
+        Console.Write("What is your  first name? ");
+        String name = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        String lastname = Console.ReadLine();
+    
+
+       Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
+
     }
 }
