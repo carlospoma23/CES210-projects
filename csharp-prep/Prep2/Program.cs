7d6d8b110877df381a2958d09e479e20a8a26053
carlospoma23 <carlospoma23@gmail.com>
2023-04-19 09:16:09 -0600

    final prep 02 - week1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Welcome to Program that determines the letter grade for a course");
+        Console.WriteLine();
+        Console.Write("What is your grade percentage in this course: ");
+        double grade=double.Parse(Console.ReadLine());
+        String letter;
+
+        if (grade>=90)
+        {
+        letter="A";
+        }
+        else if(grade>=80)
+        {
+        letter="B";
+        }
+        else if(grade>=70)
+        {
+        letter="C";
+        }
+        else if(grade>=60)
+        {
+        letter="D";
+        }
+        else
+        {
+        letter="F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        
+
+        if (letter=="A" || letter=="B" || letter=="C")
+        {
+            Console.WriteLine($"Congratulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine($"I am so sorry! You didn't passe the course! Please come back you can do it!");
+        }
+        
     }
 }
