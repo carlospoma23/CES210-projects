ec7533320d1ab620fce03b3ea00bf321f4e61923
carlospoma23 <carlospoma23@gmail.com>
2023-05-20 17:31:27 -0600

    finshing week 4

---
+++ prove/Develop03/text.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HideText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "People often try to memorize poems or passages of scripture. One of the challenges they encounter is that they want to hide the scripture while they are practicing, but they may not be able to recite the whole scripture from memory just yet.";
+            string hiddenText = new string('*', text.Length);
+
+            Console.WriteLine(hiddenText);
+
+            Console.ReadKey();
+        }
+    }
+}
