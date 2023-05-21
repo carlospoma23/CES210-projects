b8de9481f03e91efe80deb27ecac2d6811099551
carlospoma23 <carlospoma23@gmail.com>
2023-05-20 23:04:51 -0600

    Updating Learning 03 done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction myFraction1 = new Fraction();
+
+        Console.WriteLine("First Test");
+
+        Console.WriteLine(myFraction1.GetFractionString());
+        Console.WriteLine(myFraction1.GetDecimalValue());
+
+
+        Console.WriteLine("Second Test");
+        Fraction myFraction2 = new Fraction(6);
+
+        Console.WriteLine(myFraction2.GetFractionString());
+        Console.WriteLine(myFraction2.GetDecimalValue());
+
+
+        Console.WriteLine("Third Test");
+        Fraction myFraction3 = new Fraction(6, 7);
+
+
+        Console.WriteLine(myFraction3.GetFractionString());
+        Console.WriteLine(myFraction3.GetDecimalValue());
+
+        Console.WriteLine("Fourth Test");
+        Fraction myFraction4 = new Fraction(1, 3);
+        Console.WriteLine(myFraction4.GetFractionString());
+        Console.WriteLine(myFraction4.GetDecimalValue());
+
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,12 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        string text = "People often try to memorize poems or passages of scripture. One of the challenges they encounter is that they want to hide the scripture while they are practicing, but they may not be able to recite the whole scripture from memory just yet.";
-        string hiddenText = new string('*', text.Length);
 
-        Console.WriteLine(hiddenText);
 
-        Console.ReadKey();
 
     }
+
 }
